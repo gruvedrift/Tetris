edc940ee8193954f62d709c28bf907e283f4ff2e
William <williamad90@gmail.com>
2024-08-13 16:00:25 +0200

    Small update
    Testing new shape object with separate collision and pivot coordinate.

--- src/assets/Utils.tsx
+++ src/assets/Utils.tsx
@@ -4,109 +4,125 @@ import {X_AXIS_DIMENTION, Y_AXIS_DIMENTION} from "../App.tsx";
 import {Coordinate, Shape, ShapeV2} from "./types";
 
 export function coordinateLimit(num: number): boolean {
-  return num > 9 || num < 0;
+    return num > 9 || num < 0;
 }
 
 // Helper function to make the ternary styling easier to read
 export function getStyling(collisionCell: boolean, stackCell: boolean, paddingCell: boolean): string {
-  switch (true) {
-    case collisionCell && paddingCell:
-      return styles.collisioncell
-    case !collisionCell && paddingCell:
-      return styles.paddingcell
-    case collisionCell && stackCell: // Bottom of the page.
-      return styles.stackcell
-    case !collisionCell && stackCell: // Cell or shape has reached bottom, change color.
-      return styles.stackcell
-    case collisionCell && !stackCell: // Active cell moving around with arrows.
-      return styles.collisioncell
-    case !collisionCell && !stackCell: // Regular background cell, no special styling.
-      return styles.gridcell
-    default:
-      return styles.gridcell // Default case is just give regular styling
-  }
+    switch (true) {
+        case collisionCell && paddingCell:
+            return styles.collisioncell
+        case !collisionCell && paddingCell:
+            return styles.paddingcell
+        case collisionCell && stackCell: // Bottom of the page.
+            return styles.stackcell
+        case !collisionCell && stackCell: // Cell or shape has reached bottom, change color.
+            return styles.stackcell
+        case collisionCell && !stackCell: // Active cell moving around with arrows.
+            return styles.collisioncell
+        case !collisionCell && !stackCell: // Regular background cell, no special styling.
+            return styles.gridcell
+        default:
+            return styles.gridcell // Default case is just give regular styling
+    }
 }
 
 
 // Basic check of shape will be 'out of bound' on next move.
 export function coordinateLimitForShape(shape: Shape): boolean {
-  return shape.coordinateList.some(coordinate =>
-    coordinate.x_axis > X_AXIS_DIMENTION - 1 || coordinate.y_axis > Y_AXIS_DIMENTION - 1 ||
-    coordinate.x_axis < 0 || coordinate.y_axis < 0
-  )
+    return shape.coordinateList.some(coordinate =>
+        coordinate.x_axis > X_AXIS_DIMENTION - 1 || coordinate.y_axis > Y_AXIS_DIMENTION - 1 ||
+        coordinate.x_axis < 0 || coordinate.y_axis < 0
+    )
 }
 
 // Returns true if any coordinate in shape is 'touching' the bottom stack.
 // True on coordinate y-axis - 1 to 'stack' on top.
 export function isShapeTouchingStack(shape: Shape, stackCoordinates: Coordinate[]): boolean {
-  return shape.coordinateList.some(shapeCoordinates =>
-    stackCoordinates.some(stackCoordinates =>
-      shapeCoordinates.x_axis === stackCoordinates.x_axis &&
-      shapeCoordinates.y_axis === stackCoordinates.y_axis - 1
+    return shape.coordinateList.some(shapeCoordinates =>
+        stackCoordinates.some(stackCoordinates =>
+            shapeCoordinates.x_axis === stackCoordinates.x_axis &&
+            shapeCoordinates.y_axis === stackCoordinates.y_axis - 1
+        )
     )
-  )
 }
 
 // Returns true if any coordinate in shape is 'touching' bottom of the board.
 // True on coordinate y-axis -1 to 'stack' on bottom.
 export function isShapeAtBottomOfGameBoard(shape: Shape): boolean {
-  return shape.coordinateList.some(coordinate =>
-    coordinate.y_axis === Y_AXIS_DIMENTION - 1
-  )
+    return shape.coordinateList.some(coordinate =>
+        coordinate.y_axis === Y_AXIS_DIMENTION - 1
+    )
 }
 
 // Helper function to generate new shapes.
 // TODO make shape hold color also?
 export function generateNewShape(): Shape {
-  return {
-    coordinateList: [
-      {x_axis: 4, y_axis: 0},
-      {x_axis: 4, y_axis: 1},
-      {x_axis: 5, y_axis: 0},
-      {x_axis: 5, y_axis: 1},
-    ]
-  }
+    return {
+        coordinateList: [
+            {x_axis: 4, y_axis: 0},
+            {x_axis: 4, y_axis: 1},
+            {x_axis: 5, y_axis: 0},
+            {x_axis: 5, y_axis: 1},
+        ]
+    }
 }
 
 // Generate L shape for testing
 export function generateLShape(): Shape {
-  return {
-    coordinateList: [
-      {x_axis: 4, y_axis: 0},
-      {x_axis: 4, y_axis: 1},
-      {x_axis: 4, y_axis: 2},
-      {x_axis: 5, y_axis: 2},
-    ]
-  }
+    return {
+        coordinateList: [
+            {x_axis: 4, y_axis: 0},
+            {x_axis: 4, y_axis: 1},
+            {x_axis: 4, y_axis: 2},
+            {x_axis: 5, y_axis: 2},
+        ]
+    }
 }
 
 // Generate L shape for testing
 export function generateLShapeV2(): ShapeV2 {
-  return {
-    // Padding for shape
-    coordinates: [
-      {x_axis: 3, y_axis: 0},
-      {x_axis: 3, y_axis: 1},
-      {x_axis: 3, y_axis: 2},
-      {x_axis: 4, y_axis: 0},
-      {x_axis: 4, y_axis: 1},
-      {x_axis: 4, y_axis: 2},
-      {x_axis: 5, y_axis: 0},
-      {x_axis: 5, y_axis: 1},
-      {x_axis: 5, y_axis: 2},
-    ],
-    collisionCoordinates: [
-      {x_axis: 4, y_axis: 0},
-      {x_axis: 4, y_axis: 1},
-      {x_axis: 4, y_axis: 2},
-      {x_axis: 5, y_axis: 2},
-    ],
-    pivotPointCoordinate: 4,
-  }
+    return {
+        // Padding for shape
+        coordinates: [
+            {x_axis: 3, y_axis: 0},
+            {x_axis: 3, y_axis: 1},
+            {x_axis: 3, y_axis: 2},
+            {x_axis: 4, y_axis: 0},
+            {x_axis: 4, y_axis: 1},
+            {x_axis: 4, y_axis: 2},
+            {x_axis: 5, y_axis: 0},
+            {x_axis: 5, y_axis: 1},
+            {x_axis: 5, y_axis: 2},
+        ],
+        collisionCoordinates: [
+            {x_axis: 4, y_axis: 0},
+            {x_axis: 4, y_axis: 1},
+            {x_axis: 4, y_axis: 2},
+            {x_axis: 5, y_axis: 2},
+        ],
+        pivotPointCoordinate: 4,
+    }
 }
 
+// new x = pivot x + (Y - pivot Y)
+// nex = pivot y - (x - pivot x)
+// TODO clean up this mess, looks like shit unfortunately
 export function rotateLshapeV2(shape: ShapeV2): ShapeV2 {
-  return generateLShapeV2()
+    const pivotCoordinate = shape.coordinates[shape.pivotPointCoordinate]
+    console.log('Pivot coordinate', pivotCoordinate)
+    return {
+        coordinates: shape.coordinates,
+        collisionCoordinates: shape.collisionCoordinates.map(coordinate => {
+            const rotatedX = pivotCoordinate.x_axis -  (coordinate.y_axis - pivotCoordinate.y_axis)
+            const rotatedY = pivotCoordinate.y_axis +  (coordinate.x_axis - pivotCoordinate.x_axis)
+            return {
+                x_axis: rotatedX,
+                y_axis: rotatedY
+            }
+        }),
+        pivotPointCoordinate: shape.pivotPointCoordinate
+    }
 }
 
 
@@ -114,22 +130,22 @@ export function rotateLshapeV2(shape: ShapeV2): ShapeV2 {
 // Simple math formula from geometry (x, y) - > (y, -x).
 // Add 9 to x-coordinate and 0 to y coordinate to adjust for no negative coordinates
 export function rotateLShape(shape: Shape): Shape {
-  const pivot = shape.coordinateList[3]
-  console.log('Pivot point', pivot)
-  return {
-    coordinateList: shape.coordinateList.map(coordinate => {
-      const translatedX = coordinate.x_axis = pivot.x_axis
-      const translatedY = coordinate.y_axis = pivot.y_axis
+    const pivot = shape.coordinateList[3]
+    console.log('Pivot point', pivot)
+    return {
+        coordinateList: shape.coordinateList.map(coordinate => {
+            const translatedX = coordinate.x_axis = pivot.x_axis
+            const translatedY = coordinate.y_axis = pivot.y_axis
 
-      const rotatedX = translatedY
-      const rotatedY = -translatedX
-      return {
-        x_axis: rotatedX + pivot.x_axis,
-        y_axis: ( rotatedY + pivot.y_axis * -1) + 9
-        // y_axis: (coordinate.x_axis * -1) + 9,
-      }
-    })
-  }
+            const rotatedX = translatedY
+            const rotatedY = -translatedX
+            return {
+                x_axis: rotatedX + pivot.x_axis,
+                y_axis: (rotatedY + pivot.y_axis * -1) + 9
+                // y_axis: (coordinate.x_axis * -1) + 9,
+            }
+        })
+    }
 }
 
 
@@ -139,31 +155,31 @@ export function rotateLShape(shape: Shape): Shape {
 // temporarily set to return true / false and clear bottom.
 // TODO extend to clear multiple rows at a time to manage some scoring system for multiple rows on stack solved.
 export function numberOfRowsToClear(stackCoordinates: Coordinate[]): boolean {
-  return BOTTOM_ROW_COORDINATES.coordinateList.every(bottomRowCoordinates =>
-    stackCoordinates.some(coordinate =>
-      bottomRowCoordinates.x_axis === coordinate.x_axis &&
-      bottomRowCoordinates.y_axis === coordinate.y_axis
+    return BOTTOM_ROW_COORDINATES.coordinateList.every(bottomRowCoordinates =>
+        stackCoordinates.some(coordinate =>
+            bottomRowCoordinates.x_axis === coordinate.x_axis &&
+            bottomRowCoordinates.y_axis === coordinate.y_axis
+        )
     )
-  )
 }
 
 
 // TODO implement
 export function adjustStackAfterRowClear(stackCoordinates: Coordinate[]): Coordinate[] {
-  return []
+    return []
 }
 
 export const BOTTOM_ROW_COORDINATES = {
-  coordinateList: [
-    {x_axis: 0, y_axis: 9},
-    {x_axis: 1, y_axis: 9},
-    {x_axis: 2, y_axis: 9},
-    {x_axis: 3, y_axis: 9},
-    {x_axis: 4, y_axis: 9},
-    {x_axis: 5, y_axis: 9},
-    {x_axis: 6, y_axis: 9},
-    {x_axis: 7, y_axis: 9},
-    {x_axis: 8, y_axis: 9},
-    {x_axis: 9, y_axis: 9},
-  ]
+    coordinateList: [
+        {x_axis: 0, y_axis: 9},
+        {x_axis: 1, y_axis: 9},
+        {x_axis: 2, y_axis: 9},
+        {x_axis: 3, y_axis: 9},
+        {x_axis: 4, y_axis: 9},
+        {x_axis: 5, y_axis: 9},
+        {x_axis: 6, y_axis: 9},
+        {x_axis: 7, y_axis: 9},
+        {x_axis: 8, y_axis: 9},
+        {x_axis: 9, y_axis: 9},
+    ]
 }
--- src/game-board/GameBoard.tsx
+++ src/game-board/GameBoard.tsx
@@ -1,12 +1,12 @@
 import styles from './GameBoard.module.scss';
 import React, {useEffect, useState} from "react";
 import {
-  BOTTOM_ROW_COORDINATES, coordinateLimit,
-  coordinateLimitForShape, generateLShape, generateLShapeV2,
-  generateNewShape,
-  isShapeAtBottomOfGameBoard,
-  isShapeTouchingStack,
-  numberOfRowsToClear, rotateLShape, rotateLshapeV2
+    BOTTOM_ROW_COORDINATES, coordinateLimit,
+    coordinateLimitForShape, generateLShape, generateLShapeV2,
+    generateNewShape,
+    isShapeAtBottomOfGameBoard,
+    isShapeTouchingStack,
+    numberOfRowsToClear, rotateLShape, rotateLshapeV2
 } from "../assets/Utils";
 import GridCell from "../GridCell/GridCell";
 import {X_AXIS_DIMENTION, Y_AXIS_DIMENTION} from "../App.tsx";
@@ -16,11 +16,11 @@ import {Color, Coordinate, Shape, ShapeV2} from "../assets/types.tsx";
 
 // TODO consider adding useMemo in order to not re-render the whole fucking page each time
 const GameBoard: React.FC = () => {
-  return (
-    <div className={styles.gameboard}>
-      <Grid/>
-    </div>
-  )
+    return (
+        <div className={styles.gameboard}>
+            <Grid/>
+        </div>
+    )
 }
 
 export default GameBoard
@@ -29,192 +29,194 @@ export default GameBoard
 // Most of the game logic happens within this grid component
 const Grid: React.FC = () => {
 
-  const test_stack_shape: Shape = {
-    coordinateList: [
-      {x_axis: 4, y_axis: 8},
-      {x_axis: 5, y_axis: 8},
-      {x_axis: 4, y_axis: 9},
-      {x_axis: 5, y_axis: 9},
-    ]
-  }
-  const test_shape_square: Shape = {
-    coordinateList: [
-      {x_axis: 8, y_axis: 2},
-      {x_axis: 9, y_axis: 2},
-      {x_axis: 8, y_axis: 3},
-      {x_axis: 9, y_axis: 3},
-    ]
-  }
-
-
-  // State to hold the currently active ' moving ' shape
-  const [activeShape, setActiveShape] = useState<Shape>({coordinateList: []})
-  // State to hold the 'stack' of tiles at the bottom of the game board.
-  const [stackCoordinates, setStackCoordinates] = useState<Coordinate[]>([])
-
-  // New and improoved??
-  const [activeShapeV2, setActiveShapeV2] = useState<ShapeV2>(
-    // {coordinates: [], collisionCoordinates: [], pivotPointCoordinate: 0}
-    generateLShapeV2
-  )
-
-
-  const handleRotateShape= (event: KeyboardEvent) => {
-    console.log('Called')
-    setActiveShapeV2(prevState => {
-      let updatedCollisionCoordinates: Coordinate[]
-      let updatedPaddingCoordinates: Coordinate[]
-      switch (event.key) {
-        case 'ArrowUp':
-          updatedCollisionCoordinates = rotateLshapeV2(prevState).collisionCoordinates
-          updatedPaddingCoordinates = rotateLshapeV2(prevState).coordinates
-          break;
-        default:
-          return prevState
-      }
-      return {
-        coordinates: updatedPaddingCoordinates,
-        collisionCoordinates: updatedCollisionCoordinates,
-        pivotPointCoordinate: prevState.pivotPointCoordinate
-      }
-    })
-  }
-
-  // TODO add some key event for rotating shape
-  /*
-  * Handles updating x-axis coordinates for the active shape on game board.
-  * Checks for out of bounds ( GameBoard x-axis limit )
-  * **/
-  const handleMoveShape = (event: KeyboardEvent) => {
-
-    setActiveShape(prevState => {
-      let updatedCoordinates: Coordinate[]
-      switch (event.key) {
-        case 'ArrowLeft':
-          updatedCoordinates = prevState.coordinateList.map(coordinate => (
-            {...coordinate, x_axis: coordinate.x_axis  -1}
-          ))
-          break;
-        case 'ArrowRight':
-          updatedCoordinates = prevState.coordinateList.map(coordinate => {
-           return {...coordinate, x_axis: coordinate.x_axis + 1 }
-          })
-          break;
-        case 'ArrowDown':
-          updatedCoordinates = prevState.coordinateList.map(coordinate => (
-            {...coordinate, y_axis: coordinate.y_axis + 1 }
-          ))
-          break;
-        case 'ArrowUp':
-          updatedCoordinates = prevState.coordinateList.map(coordinate => (
-            {...coordinate, y_axis: coordinate.y_axis - 1 }
-          ))
-          break;
-        case 'x':
-          updatedCoordinates = rotateLShape(prevState).coordinateList
-          break;
-        default:
-          return prevState
-
-      }
-
-      // Only update coordinates if shape is not out of bounds on next arrow move
-      if (!coordinateLimitForShape({coordinateList: updatedCoordinates})) {
-        // console.log('Update coordinates are: ', updatedCoordinates)
-        return {coordinateList: updatedCoordinates}
-      } else {
-        return prevState
-      }
-    });
-  };
-
-  // Add coordinates to stack if they have reached the bottom of the game grid. Also, empty the active shape state
-  const addToStack = (shape: Shape) => {
-    if (isShapeTouchingStack(shape, stackCoordinates) || isShapeAtBottomOfGameBoard(shape)) {
-      console.log('Touching!!!!')
-      const updateState = stackCoordinates.concat(shape.coordinateList)
-      setStackCoordinates(updateState)
-      setActiveShape({coordinateList: []})
+    const test_stack_shape: Shape = {
+        coordinateList: [
+            {x_axis: 4, y_axis: 8},
+            {x_axis: 5, y_axis: 8},
+            {x_axis: 4, y_axis: 9},
+            {x_axis: 5, y_axis: 9},
+        ]
     }
-  }
-
-  const createNewShape = () => {
-    if (activeShape.coordinateList.length === 0) {
-      setActiveShape(generateLShape)
+    const test_shape_square: Shape = {
+        coordinateList: [
+            {x_axis: 8, y_axis: 2},
+            {x_axis: 9, y_axis: 2},
+            {x_axis: 8, y_axis: 3},
+            {x_axis: 9, y_axis: 3},
+        ]
     }
-  }
 
-  const clearBottomRow = () => {
-    if(numberOfRowsToClear(stackCoordinates)) {
-      console.log('Clear bottom!!!')
-      const updatedStackCoordinates = stackCoordinates.filter(coordinate =>
-        !BOTTOM_ROW_COORDINATES.coordinateList.some(bottomRowCoordinate =>
-          coordinate.x_axis === bottomRowCoordinate.x_axis &&
-          coordinate.y_axis === bottomRowCoordinate.y_axis
-        )
-      )
-      updatedStackCoordinates.forEach(coordinate =>
-        coordinate.y_axis += 1
-      )
-      setStackCoordinates(updatedStackCoordinates)
+
+    // State to hold the currently active ' moving ' shape
+    const [activeShape, setActiveShape] = useState<Shape>({coordinateList: []})
+    // State to hold the 'stack' of tiles at the bottom of the game board.
+    const [stackCoordinates, setStackCoordinates] = useState<Coordinate[]>([])
+
+    // New and improoved??
+    const [activeShapeV2, setActiveShapeV2] = useState<ShapeV2>(
+        // {coordinates: [], collisionCoordinates: [], pivotPointCoordinate: 0}
+        generateLShapeV2
+    )
+
+
+    const handleRotateShape = (event: KeyboardEvent) => {
+        console.log('Called')
+        setActiveShapeV2(prevState => {
+            let updatedCollisionCoordinates: Coordinate[]
+            let updatedPaddingCoordinates: Coordinate[]
+            switch (event.key) {
+                case 'ArrowUp':
+                    console.log('Previous:', prevState.collisionCoordinates)
+                    const updatedShape = rotateLshapeV2(prevState)
+                    updatedCollisionCoordinates = updatedShape.collisionCoordinates
+                    updatedPaddingCoordinates = updatedShape.coordinates
+                    console.log('New: ', updatedShape.collisionCoordinates)
+                    break;
+                default:
+                    return prevState
+            }
+            return {
+                coordinates: updatedPaddingCoordinates,
+                collisionCoordinates: updatedCollisionCoordinates,
+                pivotPointCoordinate: prevState.pivotPointCoordinate
+            }
+        })
     }
-  }
 
+    // TODO add some key event for rotating shape
+    /*
+    * Handles updating x-axis coordinates for the active shape on game board.
+    * Checks for out of bounds ( GameBoard x-axis limit )
+    * **/
+    const handleMoveShape = (event: KeyboardEvent) => {
 
+        setActiveShape(prevState => {
+            let updatedCoordinates: Coordinate[]
+            switch (event.key) {
+                case 'ArrowLeft':
+                    updatedCoordinates = prevState.coordinateList.map(coordinate => (
+                        {...coordinate, x_axis: coordinate.x_axis - 1}
+                    ))
+                    break;
+                case 'ArrowRight':
+                    updatedCoordinates = prevState.coordinateList.map(coordinate => {
+                        return {...coordinate, x_axis: coordinate.x_axis + 1}
+                    })
+                    break;
+                case 'ArrowDown':
+                    updatedCoordinates = prevState.coordinateList.map(coordinate => (
+                        {...coordinate, y_axis: coordinate.y_axis + 1}
+                    ))
+                    break;
+                case 'ArrowUp':
+                    updatedCoordinates = prevState.coordinateList.map(coordinate => (
+                        {...coordinate, y_axis: coordinate.y_axis - 1}
+                    ))
+                    break;
+                case 'x':
+                    updatedCoordinates = rotateLShape(prevState).coordinateList
+                    break;
+                default:
+                    return prevState
 
-  useEffect(() => {
-    // window.addEventListener('keydown', handleMoveShape)
-    window.addEventListener('keydown', handleRotateShape)
-    // addToStack(activeShape)
-    // createNewShape()
-    // clearBottomRow()
-    return () => {
-      // window.removeEventListener('keydown', handleMoveShape);
-      window.removeEventListener('keydown', handleRotateShape)
+            }
+
+            // Only update coordinates if shape is not out of bounds on next arrow move
+            if (!coordinateLimitForShape({coordinateList: updatedCoordinates})) {
+                // console.log('Update coordinates are: ', updatedCoordinates)
+                return {coordinateList: updatedCoordinates}
+            } else {
+                return prevState
+            }
+        });
+    };
+
+    // Add coordinates to stack if they have reached the bottom of the game grid. Also, empty the active shape state
+    const addToStack = (shape: Shape) => {
+        if (isShapeTouchingStack(shape, stackCoordinates) || isShapeAtBottomOfGameBoard(shape)) {
+            console.log('Touching!!!!')
+            const updateState = stackCoordinates.concat(shape.coordinateList)
+            setStackCoordinates(updateState)
+            setActiveShape({coordinateList: []})
+        }
     }
-  }, [activeShapeV2])
 
-
-  const row = Y_AXIS_DIMENTION;
-  const column = X_AXIS_DIMENTION;
-  const grid = []
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < column; j++) {
-
-      // Calculate if coordinate is matching the stack state coordinates
-      const stackCoordinate = stackCoordinates?.some(coordinate =>
-        coordinate.x_axis === j && coordinate.y_axis === i
-      )
-
-      // Calculate if coordinate is matching the active shape state coordinates
-      const activeShapeCoordinate = activeShape.coordinateList.some(coordinate =>
-        coordinate.x_axis === j && coordinate.y_axis === i
-      )
-
-      const collisionCell = activeShapeV2.collisionCoordinates.some(coordinate =>
-        coordinate.x_axis === j && coordinate.y_axis === i
-      )
-
-      const paddingCell = activeShapeV2.coordinates.some(coordinate =>
-        coordinate.x_axis === j && coordinate.y_axis === i
-      )
-      // Could need to actually move all the 'active' cells to the left and the right when < >
-      // Need then to 'lock' the active in shape cells that has reached the bottom of the game screen.
-
-      grid.push(
-        <GridCell
-          row={i}
-          column={j}
-          collisionCell={collisionCell}
-          stackCell={stackCoordinate}
-          paddingCell={paddingCell}
-        />
-      )
+    const createNewShape = () => {
+        if (activeShape.coordinateList.length === 0) {
+            setActiveShape(generateLShape)
+        }
     }
-  }
-  return (
-    <div className={styles.gridcontainer}>
-      {grid}
-    </div>
-  )
+
+    const clearBottomRow = () => {
+        if (numberOfRowsToClear(stackCoordinates)) {
+            console.log('Clear bottom!!!')
+            const updatedStackCoordinates = stackCoordinates.filter(coordinate =>
+                !BOTTOM_ROW_COORDINATES.coordinateList.some(bottomRowCoordinate =>
+                    coordinate.x_axis === bottomRowCoordinate.x_axis &&
+                    coordinate.y_axis === bottomRowCoordinate.y_axis
+                )
+            )
+            updatedStackCoordinates.forEach(coordinate =>
+                coordinate.y_axis += 1
+            )
+            setStackCoordinates(updatedStackCoordinates)
+        }
+    }
+
+
+    useEffect(() => {
+        // window.addEventListener('keydown', handleMoveShape)
+        window.addEventListener('keydown', handleRotateShape)
+        // addToStack(activeShape)
+        // createNewShape()
+        // clearBottomRow()
+        return () => {
+            // window.removeEventListener('keydown', handleMoveShape);
+            window.removeEventListener('keydown', handleRotateShape)
+        }
+    }, [activeShapeV2])
+
+
+    const row = Y_AXIS_DIMENTION;
+    const column = X_AXIS_DIMENTION;
+    const grid = []
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+
+            // Calculate if coordinate is matching the stack state coordinates
+            const stackCoordinate = stackCoordinates?.some(coordinate =>
+                coordinate.x_axis === j && coordinate.y_axis === i
+            )
+
+            // Calculate if coordinate is matching the active shape state coordinates
+            const activeShapeCoordinate = activeShape.coordinateList.some(coordinate =>
+                coordinate.x_axis === j && coordinate.y_axis === i
+            )
+
+            const collisionCell = activeShapeV2.collisionCoordinates.some(coordinate =>
+                coordinate.x_axis === j && coordinate.y_axis === i
+            )
+
+            const paddingCell = activeShapeV2.coordinates.some(coordinate =>
+                coordinate.x_axis === j && coordinate.y_axis === i
+            )
+            // Could need to actually move all the 'active' cells to the left and the right when < >
+            // Need then to 'lock' the active in shape cells that has reached the bottom of the game screen.
+
+            grid.push(
+                <GridCell
+                    row={i}
+                    column={j}
+                    collisionCell={collisionCell}
+                    stackCell={stackCoordinate}
+                    paddingCell={paddingCell}
+                />
+            )
+        }
+    }
+    return (
+        <div className={styles.gridcontainer}>
+            {grid}
+        </div>
+    )
 }
